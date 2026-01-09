cce2986570514949ac03b198315c9d6494459b2a
Kumachan1123 <kumakei2004@gmail.com>
2026-01-09 10:52:26 +0900

    ビルド版のライブステージのライティングを最新のものに置き換え

--- Assets/Takamori/Scripts/Select/SelectSceneManager.cs
+++ Assets/Takamori/Scripts/Select/SelectSceneManager.cs
@@ -78,7 +78,7 @@ public class SelectSceneManager : SingletonMonoBehaviour<SelectSceneManager>
     private async void GameStart()
     {
         // awaitしてシーンロード処理とPlayerManagerを取得
-        var target = await SceneLoader.Load<PlayerGenerator>("PlaySceneLive");
+        var target = await SceneLoader.Load<PlayerGenerator>("PlayLiveScene");
 
         // ターゲットを取得
         if (target == null)
